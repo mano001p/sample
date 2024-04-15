d5db138c43590aacfa02a4c86baad2dc7a994f5a
mano001p <122509501+mano001p@users.noreply.github.com>
2024-04-15 12:00:30 +0530

    Update a.php

--- a.php
+++ a.php
@@ -2,21 +2,3 @@
 // CIDR notation
 $cidr = "192.168.1.0/24";
 
-
-$external_ip = file_get_contents('https://api.ipify.org');
-echo "Your external IP address is: $external_ip";
-
-
-
-// Parse CIDR
-list($ip, $subnet_mask) = explode('/', $cidr);
-
-// Calculate network address
-$network_address = long2ip(ip2long($ip) & ~((1 << (32 - $subnet_mask)) - 1));
-
-// Calculate broadcast address
-$broadcast_address = long2ip(ip2long($network_address) | ((1 << (32 - $subnet_mask)) - 1));
-
-echo "IP range start: $network_address\n";
-echo "IP range end: $broadcast_address\n";
-?>
